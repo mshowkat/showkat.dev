38f9344c3494abf28c1bcc88250035b471bb4147
mshowkat <bangleblaze@gmail.com>
2019-08-24 11:12:47 +0600

    build v

---
+++ build/precache-manifest.a29afd541da0faa2be964a223fa33465.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "399637358cc46bbbbe9d",
+    "url": "/showkat.dev/static/css/main.be075eae.chunk.css"
+  },
+  {
+    "revision": "399637358cc46bbbbe9d",
+    "url": "/showkat.dev/static/js/main.043e2e26.chunk.js"
+  },
+  {
+    "revision": "65b514bf5174204f553a",
+    "url": "/showkat.dev/static/js/runtime~main.68d2d650.js"
+  },
+  {
+    "revision": "bf4e99453fc8b37e7a48",
+    "url": "/showkat.dev/static/js/2.35e6fa64.chunk.js"
+  },
+  {
+    "revision": "17fd24ed5900d9e7f7f3aef787f761d2",
+    "url": "/showkat.dev/index.html"
+  }
+];
